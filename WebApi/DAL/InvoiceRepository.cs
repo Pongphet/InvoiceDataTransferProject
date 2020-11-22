5ad3b232d3ebf889a41881a36e42186c9aaeeff9
Pongphet Poungkitja <pongphet.poungkitja@gmail.com>
2020-11-22 15:40:04 +0700

    Created data access layer class.

---
+++ WebApi/DAL/InvoiceRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Models;
+using WebApi.Data;
+
+namespace WebApi.DAL
+{
+    public class InvoiceRepository
+    {
+        private DatabaseContext context = new DatabaseContext();
+
+        public CurrencyCode GetCurrencyCodeByCode(string code)
+        {
+            var result = context.CurrencyCode.FirstOrDefault(x => x.Code.Equals(code));
+            return result;
+        }
+        public StatusMappings GetStatusCsvFile(string status)
+        {
+            var result = context.StatusMappings.FirstOrDefault(x => x.CsvStatus.Equals(status));
+            return result;
+        }
+        public StatusMappings GetStatusXmlFile(string status)
+        {
+            var result = context.StatusMappings.FirstOrDefault(x => x.XmlStatus.Equals(status));
+            return result;
+        }
+    }
+}
